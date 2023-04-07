a14002e69b25125c5b238ad12e6bc1c687af9d9c
Maciej Duda <micioru@gmail.com>
2023-04-07 16:03:33 +0200

    better placing and correct url

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,14 +1,12 @@
 describe('Login Tests', () => {
     beforeEach(() => {
         cy.visit('/index.html'); 
+        cy.url().should('include', '/index.html');
+        cy.contains("Wersja demonstracyjna serwisu Demobank");
       });
 
     it('Type correct credentials and check if user is redirected to the dashboard', () => {
       cy.fixture('user_data.json').then((testData) => {
-        // check if we're on expected page and structure is loaded
-        cy.url().should('include', '/pulpit.html');
-        cy.contains("Wersja demonstracyjna serwisu Demobank");
-        //
         cy.get('[data-testid="login-input"]').type(testData.validUser.user_id);
         cy.get('[data-testid="password-input"]').type(testData.validUser.password);
         cy.get('[data-testid="login-button"]').click();
@@ -17,10 +15,6 @@ describe('Login Tests', () => {
     });
 
     it('Checks error message for empty fields', () => {
-        // check if we're on expected page and structure is loaded
-        cy.url().should('include', '/pulpit.html');
-        cy.contains("Wersja demonstracyjna serwisu Demobank");
-        //
         cy.get('[data-testid="login-input"]').click();
         cy.get('[data-testid="password-input"]').click();
         cy.get('[data-testid="error-login-id"]').contains("pole wymagane");
