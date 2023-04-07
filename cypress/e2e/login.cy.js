0e23dee19ed1cb8307ce2c51a6e0146d233da9c0
Maciej Duda <micioru@gmail.com>
2023-04-07 15:45:18 +0200

    Slight cleanup and baseUrl setup

--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -1,14 +1,13 @@
 describe('Login Tests', () => {
     beforeEach(() => {
-        cy.visit('https://demo-bank.vercel.app/'); 
+        cy.visit('/index.html'); 
       });
 
     it('Type correct credentials and check if user is redirected to the dashboard', () => {
       cy.fixture('user_data.json').then((testData) => {
-        cy.visit('https://demo-bank.vercel.app/');
   
-        cy.get('[data-testid="login-input"]').type(testData.user.user_id);
-        cy.get('[data-testid="password-input"]').type(testData.user.password);
+        cy.get('[data-testid="login-input"]').type(testData.validUser.user_id);
+        cy.get('[data-testid="password-input"]').type(testData.validUser.password);
         cy.get('[data-testid="login-button"]').click();
   
         cy.url().should('include', '/pulpit.html');
