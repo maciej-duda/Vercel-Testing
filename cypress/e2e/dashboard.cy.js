8f9b90a8be4b0d4eba67a50cea5de9c20477ff3d
Maciej Duda <micioru@gmail.com>
2023-04-07 17:22:08 +0200

    basic check for user and reformat of code that used wrong char

---
+++ cypress/e2e/dashboard.cy.js
@@ -0,0 +1,24 @@
+describe('Dashboard review', () => {
+
+    beforeEach(() => {
+        cy.visit('/index.html'); 
+        cy.url().should('include', '/index.html');
+        cy.contains('Wersja demonstracyjna serwisu Demobank');
+        //use aliases for selectors
+        cy.get('[data-testid="login-input"]').as('loginInput');
+        cy.get('[data-testid="password-input"]').as('passwordInput');
+        cy.get('[data-testid="login-button"]').as('loginButton');
+
+        cy.fixture('user_data.json').then((testData) => {
+            cy.get('@loginInput').type(testData.validUser.user_id);
+            cy.get('@passwordInput').type(testData.validUser.password);
+            cy.get('@loginButton').click();
+            cy.url().should('include', '/pulpit.html');
+        });
+    });
+
+    it('Checks basic information about customer', () => {
+        cy.get('#user_name').contains('Jan Demobankowy');
+        cy.get('#account_number').contains('(41 4100 1111 1111 1111 1111 0000)');
+    });
+});
--- cypress/e2e/login.cy.js
+++ cypress/e2e/login.cy.js
@@ -3,7 +3,7 @@ describe('Login Tests', () => {
     beforeEach(() => {
         cy.visit('/index.html'); 
         cy.url().should('include', '/index.html');
-        cy.contains("Wersja demonstracyjna serwisu Demobank");
+        cy.contains('Wersja demonstracyjna serwisu Demobank');
         //use aliases for selectors
         cy.get('[data-testid="login-input"]').as('loginInput');
         cy.get('[data-testid="password-input"]').as('passwordInput');
@@ -22,9 +22,9 @@ describe('Login Tests', () => {
       it('Checks error message for empty fields', () => {
           cy.get('@loginInput').click();
           cy.get('@passwordInput').click();
-          cy.get('[data-testid="error-login-id"]').contains("pole wymagane");
+          cy.get('[data-testid="error-login-id"]').contains('pole wymagane');
           cy.get('@loginInput').click();
-          cy.get('[data-testid="error-login-password"]').contains("pole wymagane");
+          cy.get('[data-testid="error-login-password"]').contains('pole wymagane');
         });
   
       it('Checks if button is disabled when no data is provided or wrong data is provided', () => {
@@ -41,8 +41,8 @@ describe('Login Tests', () => {
             cy.get('@loginInput').type(testData.invalidUser.user_id);
             cy.get('@passwordInput').type(testData.invalidUser.password);
             cy.get('@loginInput').click();
-            cy.get('[data-testid="error-login-id"]').contains("identyfikator ma min. 8 znaków");
-            cy.get('[data-testid="error-login-password"]').contains("hasło ma min. 8 znaków");
+            cy.get('[data-testid="error-login-id"]').contains('identyfikator ma min. 8 znaków');
+            cy.get('[data-testid="error-login-password"]').contains('hasło ma min. 8 znaków');
           });
         });
     });
